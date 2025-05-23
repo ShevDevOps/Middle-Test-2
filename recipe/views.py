20745ad37e7135eea38bcd7843e181b5ca43bfe4
Matyuk <Stivano@ukr.net>
2025-05-23 12:36:22 +0300

    Crerated view main

--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,6 @@
+from .models import Recipe, Category
+from django.shortcuts import render
 
+def main(request):
+    recipes = Recipe.objects.all()[:5]
+    return render(request, 'recipe/main.html', {'recipes': recipes})
