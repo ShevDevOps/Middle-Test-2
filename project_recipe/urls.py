4d73d52f3dc02802b28c7a477065755ce2e15cc8
Timofiy Honcharov <tgjvua@gmail.com>
2025-05-23 12:54:41 +0300

    categoryDetails added

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from recipe.views import category_list
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('category_list/', category_list, name="categories")
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,7 @@
+from django.shortcuts import render
+from .models import Category
+from django.db.models import Count
 
+def category_list(request):
+    categories = Category.objects.annotate(recipe_count=Count('categories'))
+    return render(request, 'category_list.html', {'categories': categories})
